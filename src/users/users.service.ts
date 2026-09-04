d0a2b467d319dfbaeaa9f1fb6575ca5055b677eb
Thomas <114309234+thomasweigert91@users.noreply.github.com>
2026-09-04 13:14:36 +0200

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,10 @@
-import * as bcrypt from 'bcrypt';
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './entities/users.entity';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UserResponseDto } from './dto/user-response.dto';
+import * as bcrypt from "bcrypt";
+import { ConflictException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./entities/users.entity";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UserResponseDto } from "./dto/user-response.dto";
 
 @Injectable()
 export class UsersService {
@@ -26,6 +26,11 @@ export class UsersService {
   }
 
   async create(createUserDto: CreateUserDto): Promise<UserResponseDto> {
+    const existingUser = await this.findUserByUsername(createUserDto.username);
+    if (existingUser) {
+      throw new ConflictException("Username already exists");
+    }
+
     const passwordHash: string = await bcrypt.hash(createUserDto.password, 10);
     const user = this.usersRepository.create({
       id: crypto.randomUUID(),
